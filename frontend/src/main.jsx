af672156419fa90361a4f16e8255ec59562e9efd
sheldondsouza <sheldondsouza860@gmail.com>
2025-09-02 21:20:39 +0530

    error debuggig 2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index'
+import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router'
 import {Toaster} from "react-hot-toast"
